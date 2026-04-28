e1b561c580e5a397d802f2314c0e5774ee77e323
Okan Ozbek <devokan@outlook.com>
2026-04-28 17:00:43 +0200

    feat: progress deque

--- include/Deque.h
+++ include/Deque.h
@@ -12,30 +12,42 @@ namespace dsa {
     public:
         Deque()
             : m_data{ static_cast<TValue*>(::operator new(m_capacity * sizeof(TValue))) }
+            , m_capacity{ DEFAULT_CAPACITY }
+            , m_size{ 0 }
+            , m_front{ 0 }
+            , m_back{ 0 }
         {
             for (std::size_t i{}; i < m_capacity; ++i) {
-                new (&m_data[i]) T{};
+                new (&m_data[i]) TValue{};
             }
         }
 
         Deque(const Deque& other)
             : m_data{ static_cast<TValue*>(::operator new(other.m_capacity * sizeof(TValue))) }
-        , m_capacity{ other.m_capacity }
-        , m_size{ other.m_size }
+            , m_capacity{ other.m_capacity }
+            , m_size{ other.m_size }
+            , m_front{ other.m_front }
+            , m_back{ other.m_back }
         {
-            for (std::size_t i{}; i < m_size; ++i) {
-                new (&m_data) TValue(other.m_data[i]);
+            for (std::size_t i{}; i < other.m_size; ++i) {
+                std::size_t index{ (other.m_front + i) % other.m_capacity - 1 };
+
+                new (&m_data[i]) TValue(other.m_data[index]);
             }
         }
 
         Deque(Deque&& other) noexcept
-            : m_data{ std::move(other.m_data) }
-        , m_capacity{ other.m_capacity }
-        , m_size{ other.m_size }
+            : m_data{ other.m_data }
+            , m_capacity{ other.m_capacity }
+            , m_size{ other.m_size }
+            , m_front{ other.m_front }
+            , m_back{ other.m_back }
         {
             other.m_data = nullptr;
             other.m_capacity = 0;
             other.m_size = 0;
+            other.m_front = 0;
+            other.m_back = 0;
         }
 
         ~Deque() {
@@ -43,6 +55,8 @@ namespace dsa {
 
             m_capacity = 0;
             m_size = 0;
+            m_front = 0;
+            m_back = 0;
         }
 
         // Revisit for ring buffer impl
@@ -57,16 +71,20 @@ namespace dsa {
             m_data = static_cast<TValue*>(::operator new(m_capacity * sizeof(TValue)));
             m_capacity = other.m_capacity;
             m_size = other.m_size;
+            m_front = other.m_front;
+            m_back = other.m_back;
 
-            for (std::size_t i{}; i < m_size; ++i) {
-                new (&m_data[i]) TValue(other.m_data[i]);
+            for (std::size_t i{}; i < other.m_size; ++i) {
+                std::size_t index{ (other.m_front + i) % other.m_capacity - 1 };
+
+                new (&m_data[i]) TValue(other.m_data[index]);
             }
 
             return *this;
         }
 
         // Revisit for ring buffer impl
-        Deque& operator=(const Deque&& other) {
+        Deque& operator=(Deque&& other) noexcept {
             if (&other == this) {
                 return *this;
             }
@@ -74,52 +92,37 @@ namespace dsa {
             Clear();
             ::operator delete(m_data);
 
-            m_data = std::move(other.m_data);
-            m_size = other.m_size;
+            m_data = other.m_data;
             m_capacity = other.m_capacity;
+            m_size = other.m_size;
+            m_front = other.m_front;
+            m_back = other.m_back;
 
             other.m_data = nullptr;
-            other.m_size = 0;
             other.m_capacity = 0;
+            other.m_size = 0;
+            other.m_front = 0;
+            other.m_back = 0;
 
             return *this;
         }
 
         // Wont work with ring buffer impl
-        TValue& operator[](std::size_t index) {
-            if (IsOutOfBounds(index)) {
-                throw std::out_of_range("Index out of range");
-            }
-
-            return m_data[index];
+        TValue& operator[](const std::size_t index) const {
+            return m_data[(m_front + index) % m_capacity];
         }
 
         // Wont work with ring buffer impl
-        TValue& At(std::size_t index) {
-            if (IsOutOfBounds(index)) {
-                throw std::out_of_range("Index out of range");
-            }
-
-            return m_data[index];
+        TValue& At(const std::size_t index) const {
+            return m_data[(m_front + index) % m_capacity];
         }
 
-        // Wont work with ring buffer impl
-        TValue& Front() {
-            if (IsEmpty()) {
-                throw std::out_of_range("Index out of range");
-            }
-
-            return m_data[0];
+        TValue& Front() const {
+            return m_data[m_front];
         }
 
-        // Wont work with ring buffer impl
-        TValue& Back() {
-            if (IsEmpty()) {
-                throw std::out_of_range("Index out of range");
-            }
-
-            // TODO figure out why this is being cringe..
-            return m_data[m_size - 1];
+        TValue& Back() const {
+            return m_data[m_back];
         }
 
         std::size_t Size() {
@@ -127,39 +130,102 @@ namespace dsa {
         }
 
         bool IsEmpty() {
-            return m_size > 0;
+            return m_size == 0;
         }
 
         // We need to implement a hybrid dynamic-array / ring-buffer style resize
         // The goal is that during the resizing we also linearize all our data
         // Meaning if our buffer was pointing at 4-12 it would now be reset to 0-8
-        void Resize() {}
-        void ShrinkToFit() {}
+        void Resize(const std::size_t newCapacity) {
+            if (newCapacity == m_capacity) {
+                return;
+            }
+
+            auto* newData{ static_cast<TValue*>(::operator new(newCapacity * sizeof(TValue))) };
+
+            for (std::size_t i{}; i < m_size; ++i) {
+                std::size_t index{ (m_front + i) % m_capacity };
+
+                new (&newData[i]) TValue(std::move(m_data[index]));
+            }
+
+            ::operator delete(m_data);
+            m_data = newData;
+
+            m_capacity = newCapacity;
+            m_front = 0;
+            m_back = m_size;
+        }
+
+        // void ShrinkToFit() {} TODO extra
 
         // Push back will just append to our end pointer + 1
-        void PushBack() {
+        void PushBack(const TValue& item) {
+            if (IsFull()) {
+                Resize(m_capacity * 2);
+            }
 
+            new (&m_data[m_back]) TValue(item);
+
+            m_back = (m_back + 1) % m_capacity;
+            ++m_size;
         }
 
         // Push front will just append to our front pointer - 1
-        void PushFront() {}
+        void PushFront(const TValue& item) {
+            if (IsFull()) {
+                Resize(m_capacity * 2);
+            }
+
+            m_front = (m_front + m_capacity - 1) % m_capacity;
+            new (&m_data[m_front]) TValue(item);
+            ++m_size;
+        }
 
         // Pop back will pop and remove the element from our end pointer
         // End pointer will now be x -= 1
-        void PopBack() {
+        TValue PopBack() {
+            if (IsEmpty()) {
+                throw std::out_of_range("Deque is empty");
+            }
 
+            std::size_t index{ m_back };
+            m_back = (m_back + m_capacity - 1) % m_capacity;
+            --m_size;
+
+            TValue value = std::move(m_data[index]);
+            m_data[index].~TValue();
+
+            return value;
         }
 
         // Pop front will pop and remove the element from our front pointer
         // Front pointer will now be x += 1
-        void PopFront() {}
+        TValue PopFront() {
+            if (IsEmpty()) {
+                throw std::out_of_range("Deque is empty");
+            }
+
+            std::size_t index{ m_front };
+            m_front = (m_front + 1) % m_capacity;
+            --m_size;
+
+            TValue value = std::move(m_data[index]);
+            m_data[index].~TValue();
+
+            return value;
+        }
+
         void EmplaceFront() {}
         void EmplaceBack() {}
-        void Swap() {}
+
+        void Swap(const Deque& other) {}
 
         void Clear() {
             for (std::size_t i{}; i < m_size; ++i) {
-                m_data[i].~T();
+                std::size_t index{ (m_front + i) % m_capacity };
+
+                m_data[index].~TValue();
             }
 
             m_size = 0;
@@ -170,13 +236,17 @@ namespace dsa {
         constexpr static int DEFAULT_CAPACITY{ 10 };
 
         TValue* m_data{ nullptr };
-        std::size_t m_capacity{ DEFAULT_CAPACITY };
-        std::size_t m_size{ 0 };
-        std::size_t m_front{ 0 };
-        std::size_t m_back{ 0 };
+        std::size_t m_capacity;
+        std::size_t m_size;
+        std::size_t m_front;
+        std::size_t m_back;
 
-        bool IsOutOfBounds(std::size_t& index) {
+        [[nodiscard]] constexpr bool IsOutOfBounds(const std::size_t& index) const {
             return index >= m_size;
+        }
+
+        [[nodiscard]] constexpr bool IsFull() const {
+            return m_size == m_capacity;
         }
     };
 }
