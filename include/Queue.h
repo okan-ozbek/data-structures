b947a6bc57072eff33228a09b4b97742d86a74c5
Okan Ozbek <devokan@outlook.com>
2026-04-30 00:30:46 +0200

    feat: added comments

--- include/Queue.h
+++ include/Queue.h
@@ -10,20 +10,52 @@
 
 
 namespace dsa {
+    /**
+     * Queue is a data structure that follows the First-In-First-Out (FIFO) principle,
+     * where the first element added to the queue. Under the hood this class is a wrapper
+     * for the Deque class, limiting its scope to only allow Queue defined operations
+     *
+     * @tparam TValue
+     */
     template<typename TValue>
     class Queue {
     public:
+        /**
+         * Default constructor
+         */
         Queue()
             : m_deque{} {}
 
+        /**
+         * Copy constructor
+         *
+         * Time complexity: O(n) as it has to copy all the elements from the other Queue&
+         * @param other
+         */
         Queue(const Queue& other)
             : m_deque{ other.m_deque } {}
 
+        /**
+         * Move constructor
+         *
+         * Time complexity: O(1) as it only needs to move the Deque
+         * @param other
+         */
         Queue(Queue&& other) noexcept
             : m_deque{ std::move(other.m_deque) } {}
 
+        /**
+         * Default destructor
+         */
         ~Queue() = default;
 
+        /**
+         * Copy assignment operator
+         *
+         * Time complexity: O(n) as it has to copy all the elements from the other Queue&
+         * @param other
+         * @return Queue&
+         */
         Queue& operator=(const Queue& other) {
             if (&other == this) {
                 return *this;
@@ -34,6 +66,13 @@ namespace dsa {
             return *this;
         }
 
+        /**
+         * Move assignment operator
+         *
+         * Time complexity: O(1) as it only needs to move the Deque
+         * @param other
+         * @return Queue&
+         */
         Queue& operator=(Queue&& other) noexcept {
             if (&other == this) {
                 return *this;
@@ -44,35 +83,83 @@ namespace dsa {
             return *this;
         }
 
+        /**
+         * Check if the queue is empty
+         *
+         * Time complexity: O(1)
+         * @return bool
+         */
         bool IsEmpty() const {
             return m_deque.IsEmpty();
         }
 
+        /**
+         * Get the size of the queue
+         *
+         * @return std::size_t
+         */
         std::size_t Size() const {
             return m_deque.Size();
         }
 
+        /**
+         * Get the first element in the queue
+         *
+         * Time complexity: O(1)
+         * @return TValue&
+         */
         TValue& Front() const {
             return m_deque.Front();
         }
 
+        /**
+         * Get the last element in the queue
+         *
+         * Time complexity: O(1)
+         * @return TValue&
+         */
         TValue& Back() const {
             return m_deque.Back();
         }
 
+        /**
+         * Push a new element to the back of the queue
+         *
+         * Time complexity: O(1) if the deque is not full, and O(n) if the deque is full since it needs to resize the deque.
+         * @param item
+         */
         void Push(const TValue& item) {
             m_deque.PushBack(item);
         }
 
+        /**
+         * Push a new element to the back of the queue
+         *
+         * Time complexity: O(1) if the deque is not full, and O(n) if the deque is full since it needs to resize the deque.
+         * @param args
+         */
         template<typename... TArgs>
         void Emplace(TArgs&&... args) {
             m_deque.EmplaceBack(std::forward<TArgs>(args)...);
         }
 
+        /**
+         * Pop the oldest element (the front) from the queue and return it. If the queue is empty,
+         * it will throw an out_of_range exception.
+         *
+         * Time complexity: O(1)
+         * @return TValue
+         */
         TValue Pop() {
             return m_deque.PopFront();
         }
 
+        /**
+         * Swap the two queues
+         *
+         * Time complexity: O(1) as it only moves data from and to each other using std::swap()
+         * @param other
+         */
         void Swap(Queue& other) {
             m_deque.Swap(other.m_deque);
         }
--- include/Stack.h
+++ include/Stack.h
@@ -13,17 +13,42 @@ namespace dsa {
     template<typename TValue>
     class Stack {
     public:
+        /**
+         * Default constructor
+         */
         Stack()
             : m_deque{} {}
 
+        /**
+         * Copy constructor
+         *
+         * Time complexity: O(n) as it has to copy all the elements from the other Stack&
+         * @param other
+         */
         Stack(const Stack& other)
             : m_deque{ other.m_deque } {}
 
+        /**
+         * Move constructor
+         *
+         * Time complexity: O(1) as it only needs to move the Deque
+         * @param other
+         */
         Stack(Stack&& other) noexcept
             : m_deque{ std::move(other.m_deque) } {}
 
+        /**
+         * Default destructor
+         */
         ~Stack() = default;
 
+        /**
+         * Copy assignment operator
+         *
+         * Time complexity: O(n) as it has to copy all the elements from the other Stack&
+         * @param other
+         * @return Stack&
+         */
         Stack& operator=(const Stack& other) {
             if (&other == this) {
                 return *this;
@@ -34,6 +59,13 @@ namespace dsa {
             return *this;
         }
 
+        /**
+         * Move assignment operator
+         *
+         * Time complexity: O(1) as it only needs to move the Deque
+         * @param other
+         * @return Stack&
+         */
         Stack& operator=(Stack&& other) noexcept {
             if (&other == this) {
                 return *this;
@@ -44,35 +76,83 @@ namespace dsa {
             return *this;
         }
 
+        /**
+         * Check if the stack is empty
+         *
+         * Time complexity: O(1)
+         * @return bool
+         */
         bool IsEmpty() const {
             return m_deque.IsEmpty();
         }
 
+        /**
+         * Get the size of the stack
+         *
+         * @return std::size_t
+         */
         std::size_t Size() const {
             return m_deque.Size();
         }
 
+        /**
+         * Get the first element in the stack
+         *
+         * Time complexity: O(1)
+         * @return TValue&
+         */
         TValue& Front() const {
             return m_deque.Front();
         }
 
+        /**
+         * Get the last element in the stack
+         *
+         * Time complexity: O(1)
+         * @return TValue&
+         */
         TValue& Back() const {
             return m_deque.Back();
         }
 
+        /**
+         * Push a new element to the back of the stack
+         *
+         * Time complexity: O(1) if the deque is not full, and O(n) if the deque is full since it needs to resize the deque.
+         * @param item
+         */
         void Push(const TValue& item) {
             m_deque.PushBack(item);
         }
 
+        /**
+         * Push a new element to the back of the stack
+         *
+         * Time complexity: O(1) if the deque is not full, and O(n) if the deque is full since it needs to resize the deque.
+         * @param args
+         */
         template<typename ...TArgs>
         void Emplace(TArgs&&... args) {
             m_deque.EmplaceBack(std::forward<TArgs>(args)...);
         }
 
+        /**
+         * Pop the newest element (the back) from the stack and return it. If the stack is empty,
+         * it will throw an out_of_range exception.
+         *
+         * Time complexity: O(1)
+         * @return TValue
+         */
         TValue Pop() {
             return m_deque.PopBack();
         }
 
+        /**
+         * Swap the two stacks
+         *
+         * Time complexity: O(1) as it only moves data from and to each other using std::swap()
+         * @param other
+         */
         void Swap(Stack& other) {
             m_deque.Swap(other.m_deque);
         }
